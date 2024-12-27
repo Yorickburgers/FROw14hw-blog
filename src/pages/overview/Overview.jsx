c6342c270c77c376feb302a7cb4efb15927ee8c7
yorickburgers <yorickburgers@live.nl>
2024-12-27 10:37:58 +0100

    Opdracht 2 done

--- src/pages/overview/Overview.jsx
+++ src/pages/overview/Overview.jsx
@@ -1,15 +1,40 @@
-import React from 'react';
-import posts from '../../constants/data.json'
+import React, {useEffect, useState} from 'react';
+// import posts from '../../constants/data.json'
 import PostTeaser from "../../components/postTeaser/PostTeaser.jsx";
 import "./Overview.css"
+import axios from 'axios'
 
 function Overview() {
+
+    const [ blogposts, setBlogposts] = useState([])
+
+
+    async function fetchAllPosts() {
+        try {
+            const response = await axios.get("http://localhost:3000/posts");
+            if (response?.data) {
+                setBlogposts(response.data);
+            }
+        }    catch (e) {
+            console.error(e);
+        }
+    }
+
+    useEffect(() => {
+        const fetchData = async () => {
+            await fetchAllPosts();
+        };
+
+        fetchData();
+
+    }, []);
+
     return (
         <>
             <h1>Alle posts</h1>
-            <h2>Er zijn {posts.length} blogposts beschikbaar</h2>
+            <h2>Er zijn {blogposts.length} blogposts beschikbaar</h2>
             <ul className="post-list">
-            {posts.map(post => (
+            {blogposts.length > 1 ? blogposts.map(post => (
                 <PostTeaser
                     title={post.title}
                     numberOfReactions={post.comments}
@@ -19,7 +44,7 @@ function Overview() {
                     postId={post.id.toString()}
                 />
                 )
-            )}
+            ) : <h3>Er is iets misgegaan bij het ophalen van de blogposts. Probeer het opnieuw.</h3>}
             </ul>
         </>
     );
